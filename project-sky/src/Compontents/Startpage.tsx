6e894a1aa8b9c8cd39c9b3b9f168cda3f4eeee65
Daniel Kjellberg <danielkjellberg@hotmail.com>
2023-01-17 10:45:47 +0100

    Startpage with working and scaling boxes for groups and rooms

--- project-sky/src/Compontents/Startpage.tsx
+++ project-sky/src/Compontents/Startpage.tsx
@@ -1,17 +1,32 @@
+import Stack from 'react-bootstrap/Stack'
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
 const Startpage = () => {
     return (
-        <div className="startpageContainer">
-          <a href="/groups">
-            <div className="selectionBox">
-                <h2 className="boxTextStartPage">Groups</h2>
-            </div>
-          </a>
-          <a href="/rooms">
-            <div className="selectionBox">
-              <h3 className="boxTextStartPage">Rooms</h3>
-            </div>
-          </a>
-        </div>
+      <Container>
+            <Stack gap={5}>
+                <Row >
+                  <Col lg={{span: 6, offset: 3}}>
+                    <a href="/groups">
+                      <div className="selectionBox">
+                            <h2 className="boxTextStartPage">Groups</h2>
+                      </div>
+                    </a>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col lg={{span: 6, offset: 3}}>
+                    <a href="/rooms">
+                      <div className="selectionBox">
+                        <h3 className="boxTextStartPage">Rooms</h3>
+                      </div>
+                    </a>
+                  </Col>
+                </Row>
+            </Stack>
+      </Container>
     )
 }
 
