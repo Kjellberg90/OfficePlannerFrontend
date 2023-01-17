40bc3fb018ae6858780c29f8d3dfccf29fc71516
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-01-17 11:41:30 +0100

    map test for groups and rooms

--- project-sky/src/Compontents/Welcome.tsx
+++ project-sky/src/Compontents/Welcome.tsx
@@ -1,7 +1,40 @@
+import { loadGroupData, loadRoomData } from "../shared/fetch/testdata";
+
 const Welcome = () => {
+
+  const {
+   jsonRoomData
+  } = loadRoomData();
+
+  const {
+   jsonGroupData
+  } = loadGroupData();
+
     return (
+      <>
         <h1>Welcome</h1>
+        <div className="rooms">
+         {
+            jsonRoomData.map(rooms => {
+               return(
+                  <h3>{rooms.roomName}</h3>
+               );
+            })
+         }
+         </div>
+         <br></br>
+         <div className="groups">
+         {
+            jsonGroupData.map(groups => {
+               return(
+                  <h3>{groups.groupName}</h3>
+               );
+            })
+         }
+         </div>
+         </>
     )
 }
+            
 
 export default Welcome;
