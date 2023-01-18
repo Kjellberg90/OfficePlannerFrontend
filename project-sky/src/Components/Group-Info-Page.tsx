f569f021e3acf817b83b3cfb72f06766cb1c4994
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-01-18 11:48:18 +0100

    display info

--- project-sky/src/Components/Group-Info-Page.tsx
+++ project-sky/src/Components/Group-Info-Page.tsx
@@ -1,6 +1,35 @@
+import { loadRoomData} from "../shared/fetch/testdata";
+import { useState } from "react"
+
+const {
+    jsonRoomData
+} = loadRoomData(); 
+
+
+function GroupHasBooking() {
+    let fakeProp = "DQT 6"
+
+}
+
+
 const GroupInfoPage = () => {
+GroupHasBooking();
     return (
-         <h1>Info page</h1> 
+        <>
+         <div className="info">
+        {
+            jsonRoomData.map(info => info.bookedBy == "DQT 6" && (
+                <h1>
+                    Team: {info.bookedBy} <br />
+                    Room: {info.roomName} <br/>
+                    Seatings: {info.seating} <br/>
+                    {info.startTime} - {info.endTime}
+                </h1>
+            )
+               
+            )}
+        </div>
+            </>     
       )
 }
 
