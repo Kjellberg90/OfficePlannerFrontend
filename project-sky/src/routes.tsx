b0986b9bba84fe59f5cdfd069fe4fb48441c3a0e
Daniel Kjellberg <danielkjellberg@hotmail.com>
2023-01-18 14:07:43 +0100

    Removed test.tsx and rounting to test

--- project-sky/src/routes.tsx
+++ project-sky/src/routes.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom"
 import Layout from "./Compontents/Layout";
 import Startpage from "./Compontents/Startpage";
-import Test from "./Compontents/Test";
 import Groups from "./Compontents/Groups";
 import RoomsPage from "./Compontents/Rooms/RoomsPage";
 
@@ -10,7 +9,6 @@ const AppRouter = () => {
         <Routes>
             <Route element={<Layout />}>
                 <Route index element={<Startpage />}/>
-                <Route path="/test" element={<Test />}/>
                 <Route path="/groups" element={<Groups/>}/>
                 <Route path="/rooms" element={<RoomsPage/>}/>
             </Route>
