52016ccd8cb1ee5d8dbfb9529f7cc5c23bff0211
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-01-18 08:24:40 +0100

    Change misspelled Compotents to Components

--- project-sky/src/routes.tsx
+++ project-sky/src/routes.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom"
-import Layout from "./Compontents/Layout";
-import Startpage from "./Compontents/Startpage";
-import Test from "./Compontents/Test";
-import Groups from "./Compontents/Groups";
-import Rooms from "./Compontents/Rooms"
+import Layout from "./Components/Layout";
+import Startpage from "./Components/Startpage";
+import Test from "./Components/Test";
+import Groups from "./Components/Groups";
+import Rooms from "./Components/Rooms";
+import GroupInfoPage from "./Components/Group-Info-Page";
 
 
 const AppRouter = () => {
@@ -13,6 +14,8 @@ const AppRouter = () => {
                 <Route index element={<Startpage />}/>
                 <Route path="/test" element={<Test/>}/>
                 <Route path="/groups" element={<Groups/>}/>
+                {/* Hårdkodat värde */}
+                    <Route path="info/DQT" element={<GroupInfoPage />} />  
                 <Route path="/rooms" element={<Rooms/>}/>
             </Route>
         </Routes>
