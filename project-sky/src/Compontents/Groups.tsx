5e74d7871193efffc61e21977b874c10e3d26522
Daniel Kjellberg <danielkjellberg@hotmail.com>
2023-01-18 12:01:21 +0100

    Styling and usestate for click on grp

--- project-sky/src/Compontents/Groups.tsx
+++ project-sky/src/Compontents/Groups.tsx
@@ -1,23 +1,53 @@
 import { loadGroupData} from "../shared/fetch/testdata";
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Stack from 'react-bootstrap/Stack'
+import {useState, useEffect} from 'react'
+import Test from "./Test";
 
-const {
+
+  const {
     jsonGroupData
-   } = loadGroupData();
+  } = loadGroupData();
 
-const Group = () => {
+   const Group = () => {
+     
+     
+    //  var x = function onClickGroupName(name: string) {
+    //  var [groupName, setgroupName] = useState("")
+    //  useEffect(() => {
+    //    setgroupName("Daniel")
+    //  }, [])
+    //  console.log(groupName)   
+    // function sendGroupNameAsProp(groupName: string){
+    //   <Test groupName={"groupName"}/>
+    // }
+
+    var [groupName, setgroupName] = useState("")
+
+   
+    console.log("Gruppnamn: " + groupName)
+    
+
     return (
-        <>
-         <h1>Groups</h1>
-           <div className="groups">
-           {
-              jsonGroupData.map(groups => {
-                 return(
-                    <h3>{groups.groupName}</h3>
-                 );
-              })
-           }
-           </div>
-           </>
+        <Container>
+           <Row className="groups">
+                <Col  lg={{span: 6, offset: 3}}>
+                  <Stack gap={5}>
+                      {
+                          jsonGroupData.map(groups => {
+                            return(
+                                <div className="groupNameDiv" key={groups.groupName} onClick={() => setgroupName(groups.groupName)}>
+                                  <h3>{groups.groupName}</h3>
+                                </div>
+                            );
+                          })
+                      }
+                  </Stack>
+                </Col>
+           </Row>
+           </Container>
       )
 }
 
--- project-sky/src/Compontents/Test.tsx
+++ project-sky/src/Compontents/Test.tsx
@@ -1,4 +1,6 @@
-const Test = () => {
+const Test = (props: any) => {
+
+  console.log("Props: " + props)
     return (
         <h1>This page is only for testing branching and merging so it shall be sentenced for removal after implemented!</h1>
     )
