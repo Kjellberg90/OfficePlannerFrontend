9110c8292dc76c65930cd77d57ff0a31894d200e
Daniel Kjellberg <danielkjellberg@hotmail.com>
2023-01-30 13:00:18 +0100

    Fetch groups from backend instead of testdata.json

--- project-sky/src/Components/Groups.tsx
+++ project-sky/src/Components/Groups.tsx
@@ -1,40 +1,42 @@
-import { loadGroupData} from "../shared/fetch/testdata";
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Stack from 'react-bootstrap/Stack'
-import { Link} from "react-router-dom";
-
- 
-  const {
-    jsonGroupData
-  } = loadGroupData();
-
-  const Group = () => {  
+import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 
+const Group = () => {  
+    
+  const [groupNames, setGroups] = useState([]);
+      
+  useEffect(() => {
+    fetch(`https://localhost:7054/api/Group/GetGroups`)
+          .then(response => response.json())
+          .then(res => setGroups(res))
+  },[])
 
-    return (
-        <Container>
-           <Row className="d-flex align-items-center justify-content-center">
-                <Col  md={6}>
-                  <Stack gap={5}>
-                      {
-                          jsonGroupData.map(groups => {
-                            return(
-                                <Link to='/info' state={{group: groups.groupName}} key={groups.groupName}>
-                                  <div className=" d-flex align-items-center justify-content-center groupNameDiv" >
-                                    <h3>{groups.groupName}</h3>
-                                  </div>
-                                </Link>
-                            );
-                          })
-                      }
-                  </Stack>
-                </Col>
-           </Row>
-        </Container>
-      )
+  return (
+    <Container>
+      <Row className="d-flex align-items-center justify-content-center">
+        <Col  md={6}>
+          <Stack gap={5}>
+              {
+              groupNames.map(name => {
+                return(
+                  <Link to='/info' state={{group: name}} key={name}>
+                    <div className=" d-flex align-items-center justify-content-center groupNameDiv" >
+                      <h3>{name}</h3>
+                    </div>
+                  </Link>
+                );
+              })
+            }
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
+  )
 }
 
 export default Group;
