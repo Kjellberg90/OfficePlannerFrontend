63b582d3a89d1f07b4d6f98ac00e6a832840f921
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-02-07 13:28:05 +0100

    move backbutton to its own component

---
+++ project-sky/src/Components/Layout/LayoutButtons.tsx
@@ -0,0 +1,29 @@
+
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Container from 'react-bootstrap/Container'
+import { useLocation, useNavigate } from "react-router-dom"
+import { useState, useEffect } from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLongArrowLeft } from "@fortawesome/free-solid-svg-icons"
+
+export const BackButton = () => {
+
+    
+    const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  }
+
+
+
+    return (
+      <Container className="layoutFooter">
+        <Row className="d-flex align-items-center justify-content-center">
+          <Col md={6}>
+            <FontAwesomeIcon icon={faLongArrowLeft} className="return-arrow" onClick={goBack} />
+          </Col>
+        </Row>
+      </Container>
+    )
+  }
