d858feea64970aab94ab21e7a9ff0c74744513cb
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-02-07 13:29:13 +0100

    remove unused imports

--- project-sky/src/Components/Layout/LayoutButtons.tsx
+++ project-sky/src/Components/Layout/LayoutButtons.tsx
@@ -2,28 +2,24 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Container from 'react-bootstrap/Container'
-import { useLocation, useNavigate } from "react-router-dom"
-import { useState, useEffect } from "react"
+import { useNavigate } from "react-router-dom"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faLongArrowLeft } from "@fortawesome/free-solid-svg-icons"
 
 export const BackButton = () => {
 
-    
     const navigate = useNavigate();
-  const goBack = () => {
-    navigate(-1);
-  }
-
-
+    const goBack = () => {
+        navigate(-1);
+    }
 
     return (
-      <Container className="layoutFooter">
-        <Row className="d-flex align-items-center justify-content-center">
-          <Col md={6}>
-            <FontAwesomeIcon icon={faLongArrowLeft} className="return-arrow" onClick={goBack} />
-          </Col>
-        </Row>
-      </Container>
+        <Container className="layoutFooter">
+            <Row className="d-flex align-items-center justify-content-center">
+                <Col md={6}>
+                    <FontAwesomeIcon icon={faLongArrowLeft} className="return-arrow" onClick={goBack} />
+                </Col>
+            </Row>
+        </Container>
     )
-  }
+}
