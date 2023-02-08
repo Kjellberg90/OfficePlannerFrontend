6bdc6a2142b7bcf4bac5405f271b51c8f9c90dda
Emil Johansson <emil.johansson2@epiroc.com>
2023-02-08 11:21:24 +0100

    Added temp datePicker for admin

--- project-sky/src/Components/Admin/AdminHomePage.tsx
+++ project-sky/src/Components/Admin/AdminHomePage.tsx
@@ -2,23 +2,47 @@ import { useEffect, useState } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { useOutletContext } from 'react-router-dom';
 import RoomOverview from "../Rooms/RoomOverview";
+import { format } from "date-fns";
 
 const AdminHomePage = () => {
 
     const [rooms, setRooms] = useState([]);
-
     var currentDate: string = useOutletContext();
 
-    useEffect(() => {
-        fetch(`https://localhost:7054/api/Room/adminOverview/` + currentDate)
+    const [date, setDate] = useState(format(new Date, "yyyy-MM-dd"))
+
+    
+    const RoomOverviewFetch = () => {
+        fetch(`https://localhost:7054/api/Room/adminOverview/` + date)
         .then(response => response.json())
         .then(res => { setRooms(res);
             console.log(res);})
-    },[currentDate])
+        }
+
+    useEffect(() => {
+        RoomOverviewFetch();
+    },[])
+        
+    const onSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log(date);
+        RoomOverviewFetch();
+    }
 
 
     return (
         <Container className="adminHomePageContainer">
+            <Row className="d-flex mb-3">
+                <Col className="justify-contend-end">
+                    <h3 className="adminHeader">Weekly schedule</h3>
+                </Col>
+                <Col>
+                    <form onSubmit={onSubmit} className="d-flex w-100 justify-content-end">
+                        <input type="text" value={date} onChange={(e) => {setDate(e.target.value)}} className="adminDateInput"></input>
+                        <button type="submit" className="btn btn-secondary">Enter</button>
+                    </form>
+                </Col>
+            </Row>
             <Row>
                 <Col className='d-flex justify-content-center'>
                     <table className="adminHomeTable">
