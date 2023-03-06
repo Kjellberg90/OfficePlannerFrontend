aecb19a29ec7c070755dcaf4fba3304e039d7b45
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-03-06 12:17:03 +0100

    garbage

--- project-sky/src/Components/Admin/AdminContext.tsx
+++ project-sky/src/Components/Admin/AdminContext.tsx
@@ -1,44 +1,52 @@
-import { createContext, ReactNode } from "react";
-import { useState } from "react";
+// import { createContext, ReactNode } from "react";
+// import { useState } from "react";
 
-interface IAdminContext {
-    weekNumber: number,
-    setWeekNumber?: (activeWeek: number) => void;
+// interface IAdminContext {
+//     weekNumber: number,
+//     setWeekNumber?: (activeWeek: number) => void;
 
-    week1: Array<{weekDate: string, weekNumber: number}>,
-    week2: Array<{weekDate: string, weekNumber: number}>,
-    week3: Array<{weekDate: string, weekNumber: number}>,
-}
+//     week1: Array<{weekDate: string, weekNumber: number}>,
+//     week2: Array<{weekDate: string, weekNumber: number}>,
+//     week3: Array<{weekDate: string, weekNumber: number}>,
+// }
 
-const defaultState = {
-    weekNumber: 1,
-    week1: Array<{weekDate: "2023-01-09", weekNumber: 1}>,
-    week2: Array<{weekDate: "2023-01-16", weekNumber: 2}>,
-    week3: Array<{weekDate: "2023-01-23", weekNumber: 3}>,
-}
+// const defaultState = {
+//     weekNumber: 1,
+//     week1: Array<{weekDate: "2023-01-09", weekNumber: 1}>,
+//     week2: Array<{weekDate: "2023-01-16", weekNumber: 2}>,
+//     week3: Array<{weekDate: "2023-01-23", weekNumber: 3}>,
+// }
 
-export const AdminContext = createContext<IAdminContext>(defaultState);
+// export const AdminContext = createContext<IAdminContext>(defaultState);
 
-export const AdminProvider = ({ children }: { children: ReactNode }) => {
-    const [weekNumber, setWeekNumber] = useState(defaultState.weekNumber);
-    const [week1] = useState(defaultState.week1);
-    const [week2] = useState(defaultState.week2);
-    const [week3] = useState(defaultState.week3);
+// export const AdminProvider = ({ children }: { children: ReactNode }) => {
+//     const [weekNumber, setWeekNumber] = useState(defaultState.weekNumber);
+//     const [week1] = useState(defaultState.week1);
+//     const [week2] = useState(defaultState.week2);
+//     const [week3] = useState(defaultState.week3);
 
-    const setWeekInfo = () => {
-        setWeekNumber(weekNumber);
-    }
+//     const setWeekInfo = () => {
+//         setWeekNumber(weekNumber);
+//     }
 
+//     return (
+//         <AdminContext.Provider
+//             value={{
+//                 weekNumber,
+//                 setWeekNumber,
+//                 week1,
+//                 week2,
+//                 week3,
+//             }}>
+//             {children}
+//         </AdminContext.Provider>
+//     )
+// }
+
+const AdminContext = () => {
     return (
-        <AdminContext.Provider
-            value={{
-                weekNumber,
-                setWeekNumber,
-                week1,
-                week2,
-                week3,
-            }}>
-            {children}
-        </AdminContext.Provider>
+        null
     )
 }
+
+export default AdminContext;
