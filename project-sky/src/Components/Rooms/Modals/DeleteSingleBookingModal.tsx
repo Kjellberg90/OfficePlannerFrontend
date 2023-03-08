c864aa1cda3b10a56f5ea4ca4b34846c0aa4c430
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-03-08 10:44:59 +0100

    clean up some code

--- project-sky/src/Components/Rooms/Modals/DeleteSingleBookingModal.tsx
+++ project-sky/src/Components/Rooms/Modals/DeleteSingleBookingModal.tsx
@@ -1,12 +1,10 @@
-import { ChangeEvent, DetailedHTMLProps, Fragment, HTMLAttributes, ReactNode, RefObject, useState } from "react";
-import { Modal, ModalProps } from "react-bootstrap";
-import { Omit, BsPrefixProps } from "react-bootstrap/esm/helpers";
-
+import { ChangeEvent, Fragment } from "react";
+import { Modal} from "react-bootstrap";
 
 type DeleteSingleBookingModalProps = {
   show: boolean;
   onHide: () => void;
-  user: { date: string; userName: string; roomId: number; pinNumbers: number[];};
+  user: { date: string; userName: string; roomId: number; pinNumbers: number[]; };
   delete: () => void;
   handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
 };
@@ -20,28 +18,29 @@ export const DeleteSingleBookingModal = ({
 }: DeleteSingleBookingModalProps) => {
 
   return (
-  <Fragment>
+    <Fragment>
       <Modal
-        show={show} onHide={onHide} 
+        show={show} 
+        onHide={onHide}
         size="lg"
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
         <Modal.Header className="d-flex justify-content-center dropSeatModalHeader" closeButton>
         </Modal.Header>
-        <Modal.Body  className="d-flex justify-content-center dropSeatModalBody" >
+        <Modal.Body className="d-flex justify-content-center dropSeatModalBody" >
           <h4>Drop Booked Seat for {user.userName}</h4>
         </Modal.Body>
         <Modal.Footer className="d-flex justify-content-around dropSeatModalFooter">
-        <form>
-            <input className="pinInput" id="deletePin1" type="number" onChange={(e) => handleChange(e)}/>
-            <input className="pinInput" id="deletePin2" type="number" onChange={(e) => handleChange(e)}/>
-            <input className="pinInput" id="deletePin3" type="number" onChange={(e) => handleChange(e)}/>
-            <input className="pinInput" id="deletePin4" type="number" onChange={(e) => handleChange(e)}/>
+          <form>
+            <input className="pinInput" id="deletePin1" type="number" onChange={(e) => handleChange(e)} />
+            <input className="pinInput" id="deletePin2" type="number" onChange={(e) => handleChange(e)} />
+            <input className="pinInput" id="deletePin3" type="number" onChange={(e) => handleChange(e)} />
+            <input className="pinInput" id="deletePin4" type="number" onChange={(e) => handleChange(e)} />
           </form>
           <button className="dropButton" onClick={handleDelete}>Drop Booking</button>
         </Modal.Footer>
       </Modal>
-  </Fragment>
+    </Fragment>
   )
 }
