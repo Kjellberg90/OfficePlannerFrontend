11c354c686f1308a6c07829ca224b8189afa6c43
Emil Johansson <emil.johansson2@epiroc.com>
2023-03-08 11:35:21 +0100

    Fixed LoginCSS

--- project-sky/src/Components/Login/Login.tsx
+++ project-sky/src/Components/Login/Login.tsx
@@ -42,13 +42,13 @@ async function login(e: React.FormEvent<HTMLFormElement>) {
   return (
     <Container>
       <Row className='d-flex align-items-center justify-content-center'>
-        <Col className="text-center" md={6}>
-          <div className="d-flex align-items-center justify-content-center flex-column room-info-col pt-2 pb-2">
-          <h2>Log In</h2>
+        <Col className="text-center" md={6} >
+          <div className="d-flex align-items-center justify-content-center flex-column room-info-col pt-2 pb-2 p-4 loginRow">
+            <h2>Log In</h2>
             <form className="form-inline" onSubmit={(e) => login(e)}>
-              <input className="form-control" type="text" name="name" id="name" placeholder="Username" onChange={handleChange} value={formData.name} required/>
+              <input className="form-control m-1" type="text" name="name" id="name" placeholder="Username" onChange={handleChange} value={formData.name} required/> 
               <br/>
-              <input className="form-control" type="password" onChange={handleChange} placeholder="Password"  name="password" id="password" value={formData.password} required/>
+              <input className="form-control m-1" type="password" onChange={handleChange} placeholder="Password" name="password" id="password" value={formData.password} required/>
               <br/>
               <input className="primaryButton roomsPageSubmitButton" type="submit" id="submit" ></input>
             </form>
