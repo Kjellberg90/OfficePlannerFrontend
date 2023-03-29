fcc7ce723611614001b8dc8ed66c2b2411e0cba8
Emil Johansson <emil.johansson2@epiroc.com>
2023-03-29 16:37:00 +0200

    Fixed fetches for dynamic weeksSchedule

--- project-sky/src/shared/Fetch/AdminHomeFetches.tsx
+++ project-sky/src/shared/Fetch/AdminHomeFetches.tsx
@@ -1,8 +1,23 @@
 import axios from "axios"
 import { GetCookie } from "../CookieHandler/Cookiehandler"
 
-export const fetchGroupsOverview = (date: string) => {
-  var result = axios.get(`https://localhost:7054/api/Room/adminOverview/` + date, {
+export const fetchGroupsOverviewWeek = (week: number) => {
+  var result = axios.get(`https://localhost:7054/api/Room/adminOverviewFromWeek/` + week, {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + GetCookie("token"),
+    }
+  })
+    .then((response) => {
+      return response.data
+    })
+    .catch(err => console.log(err))
+  
+  return result
+}
+
+export const fetchGroupsOverviewDate = (date: string) => {
+  var result = axios.get(`https://localhost:7054/api/Room/adminOverviewFromDate/` + date, {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + GetCookie("token"),
