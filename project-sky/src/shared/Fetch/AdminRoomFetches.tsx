cac683ae861777be79bcc3fbbfa47b9b8b14a2f7
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-04-12 14:45:20 +0200

    polish: AdminRoomFetches.tsx

--- project-sky/src/shared/Fetch/AdminRoomFetches.tsx
+++ project-sky/src/shared/Fetch/AdminRoomFetches.tsx
@@ -2,61 +2,74 @@ import axios from "axios"
 import { GetCookie } from "../CookieHandler/Cookiehandler"
 import instance from "./baseURL"
 
-export const fetchAdminRooms = () => {
-  var result = axios.get(`${instance}api/AdminRoom/adminGetRooms`
-  , {
+export const fetchAdminRooms = async () => {
+  try {
+    const response = await axios.get(
+      `${instance}api/AdminRoom/adminGetRooms`, 
+      {
     headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + GetCookie("token"),
     }
   }
-  )
-    .then((response) => {
-      return response.data
-    })
-    .catch(err => console.log(err))
-  
-  return result
+);
+    return response.data;
+} catch (error) {
+    console.error("Error Getting Rooms: ", error);
+  }
 }
 
-export const fetchDeleteRoom = (data : any) => {
-  var result = axios.delete(`${instance}api/AdminRoom/adminDeleteRooms`, {
-    headers: {
+export const fetchDeleteRoom = async (data: any) => {
+  try {
+    const response = await axios.delete(
+      `${instance}api/AdminRoom/adminDeleteRooms`, 
+      {
+      headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
       Authorization: "Bearer " + GetCookie("token"),
-    }, 
-    data
+      },
+      data: data 
+    }
+  );
+  return response.data;
+} catch (error) {
+    console.error("Error Deleting Room: ", error);
   }
-  )
-    .catch(err => console.log(err))
-  
-    return result
 }
 
-export const fetchPutRoom = (data: any, roomId: number) => {
-  var result = axios.put(`${instance}api/AdminRoom/adminroomEditRoom?roomId=${roomId}`, data, {
-    headers: {
+export const fetchPutRoom = async (data: any, roomId: number) => {
+  try {
+    const response = await axios.put(
+      `${instance}api/AdminRoom/adminroomEditRoom?roomId=${roomId}`, data, 
+      {
+      headers: {
       "Content-Type": "application/json",
       Authorization: "Bearer " + GetCookie("token"),
+      }
     }
-  })
-  .catch(err => console.log(err))
-
-  return result
+  );
+  return response.data;
+} catch (error) {
+    console.error("Error Updating Room: ", error);
+  }
 }
 
-export const fetchPostNewRooom = (data: any) => {
-  var result = axios.post(`${instance}api/AdminRoom/adminAddRooms`, data, {
-    headers: {
-      'Accept': 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: "Bearer " + GetCookie("token"),
+export const fetchPostNewRooom = async (data: any) => {
+  try {
+    const response = await axios.post(
+      `${instance}api/AdminRoom/adminAddRooms`, data, 
+      {
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        Authorization: "Bearer " + GetCookie("token"),
+      }
     }
+  );
+    return response.data;
+  } catch (error) {
+    console.error("Error Adding Room: ", error);
   }
-  )
-    .catch(err => console.log(err))
-  
-    return result
 }
 
