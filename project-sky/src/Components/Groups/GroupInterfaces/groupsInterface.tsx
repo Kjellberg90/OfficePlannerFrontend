d11ffe7f2083ae31ea2e436f5e472302c80a56c2
Daniela Escobar <daniela.b.escobar@gmail.com>
2023-04-12 09:39:44 +0200

    fix: groupsinterface semicolons

--- project-sky/src/Components/Groups/GroupInterfaces/groupsInterface.tsx
+++ project-sky/src/Components/Groups/GroupInterfaces/groupsInterface.tsx
@@ -1,14 +1,14 @@
 interface BookedRoom {
-  id: number,
-  name: string,
-  room: number,
-  bookedBy: number
+  id: number;
+  name: string;
+  room: number;
+  bookedBy: number;
 }
 
 interface Groups {
-  id: number,
-  name: string,
-  members: number,
+  id: number;
+  name: string;
+  members: number;
   bookedRoom?: BookedRoom;
 }
 export default Groups;
